aaf60f00148a7b15b587cd58a2de4708e8f14ce2
David <david.corbalan@vanadis.es>
2021-09-21 10:48:17 +0200

    Initial Commit

---
+++ exercise2films-swiftui/exercise2films_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  exercise2films_swiftuiApp.swift
+//  exercise2films-swiftui
+//
+//  Created by David on 21/9/21.
+//
+
+import SwiftUI
+
+@main
+struct exercise2films_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
